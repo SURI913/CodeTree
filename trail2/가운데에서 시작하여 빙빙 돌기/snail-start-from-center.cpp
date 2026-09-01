d8af1f941a3b648d6e11dabb16701c61c1024ef5
이수경 <101981952+SURI913@users.noreply.github.com>
2026-09-01 17:09:00 +0900

    [Passed] 가운데에서 시작하여 빙빙 돌기 (2ms, 0MB)

---
+++ trail2/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int grid[100][100];
+
+int dx[4] ={1,0,-1,0};
+int dy[4] = {0,-1,0,1};
+
+bool IsRange(int x, int y){
+    return(x >=0 && x<n && y>=0 &&y<n && grid[y][x] == 0);
+}
+
+int main() {
+    cin >> n;
+    int check = n*n;
+
+    int nx,ny;
+    int x =n/2,y=n/2,dir=0,cnt=0;
+
+    grid[y][x] = ++cnt;
+    x = x+dx[dir];
+    y = y+dy[dir];
+
+    while(--check){
+        grid[y][x] = ++cnt;
+        //바라보고있는 방향의 
+
+        int checkdir = (dir+1)%4;
+        nx = x+dx[checkdir];
+        ny = y+dy[checkdir];
+        
+        if(IsRange(nx,ny)){
+            dir = checkdir;
+        }
+
+        x +=dx[dir];
+        y +=dy[dir];
+
+    }
+
+    for(int i =0; i<n;i++){
+        for(int j=0; j<n;j++){
+            cout << grid[i][j]<<" ";
+        }
+        cout << '\n';
+    }
+    // Please write your code here.
+
+    return 0;
+}
